29715e706ca2c75eff2d73537233123bcc40f2c1
wearrqqws <99224452+wearrqqws@users.noreply.github.com>
2022-06-23 12:44:09 -0500

    First (and probably only) commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const nebulaWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    }
+  })
+
+  nebulaWindow.loadURL('https://tutorialread.beauty')
+  nebulaWindow.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
